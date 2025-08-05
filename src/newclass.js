d956a566f2122b45973a9cc1b716bec49fb94645
Fizzyjnr <okafordaniel888@gmail.com>
2025-08-05 10:36:12 -0400

    added

---
+++ src/newclass.js
@@ -0,0 +1,15 @@
+// // npo
+// let val;
+// console.log(val);
+
+let zzz = 10
+console.log(++zzz);
+
+// for (let looping = 1; looping <= 100; looping--){
+//     console.log("im sorry")
+// }
+
+
+for (let looping1 = 1; looping1 <= 100; looping1++) {
+  console.log(looping1);
+}
